a76e1e66eb4ce2975d01c306f2d6fc302a03c2e4
raulds-fmtx <rdsflomo03@gmail.com>
2024-05-08 17:15:04 -0500

    Tests created and validated.

--- test/logo.test.js
+++ test/logo.test.js
@@ -1,2 +1,78 @@
-const Logo = require("../lib/logo.js");
 const Shapes = require("../lib/shapes.js");
+const Logo = require("../lib/logo.js");
+
+describe("Logo", () => {
+  // Test to verify that the Logo class can be instantiated.
+  describe("Instantiate", () => {
+    it("should be an instance of logo class", () => {
+      const logo = new Logo("SVG", "white", "circle", "green");
+      expect(logo).toBeInstanceOf(Logo);
+    });
+  });
+
+  // Test to verify text is assigned
+  describe("getText()", () => {
+    it("should render circle properly", () => {
+      const logo = new Logo("SVG", "white", "circle", "green");
+      expect(logo.getText()).toBe("SVG");
+    });
+  });
+
+  // Test to verify text fill is assigned
+  describe("getTextFill()", () => {
+    it("should render circle properly", () => {
+      const logo = new Logo("SVG", "white", "circle", "green");
+      expect(logo.getTextFill()).toBe("white");
+    });
+  });
+
+  // Test to verify circle shape is rendered properly
+  describe("renderShape() for shape === 'circle' ", () => {
+    it("should render circle properly", () => {
+      const circle = new Shapes.circle("green", 150, 100, 160); 
+      const rendered = circle.render();
+      const logo = new Logo("SVG", "white", "circle", "green");
+      expect(logo.renderShape()).toBe(rendered);
+    });
+  });
+
+  // Test to verify triangle shape is rendered properly
+  describe("renderShape() for shape === 'triangle' ", () => {
+    it("should render triangle properly", () => {
+      const triangle = new Shapes.triangle("green", 150, 100, 160) 
+      const rendered = triangle.render();
+      const logo = new Logo("SVG", "white", "triangle", "green");
+      expect(logo.renderShape()).toStrictEqual(rendered);
+    });
+  });
+
+  // Test to verify square shape is rendered properly
+  describe("renderShape() for shape === 'square' ", () => {
+    it("should render square properly", () => {
+      const square = new Shapes.square("green", 150, 100, 160);
+      const rendered = square.render();
+      const logo = new Logo("SVG", "white", "square", "green");
+      expect(logo.renderShape()).toStrictEqual(rendered);
+    });
+  });
+
+  // Test to verify text is rendered properly
+  describe("renderText()", () => {
+    it("should render text properly", () => {
+      const rendered = '<text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>';
+      const logo = new Logo("SVG", "white", "circle", "green");
+      expect(logo.renderText()).toBe(rendered);
+    });
+  });
+
+  // Test to verify output is rendered properly
+  describe("render()", () => {
+    it("should render output properly", () => {
+      const logo = new Logo("SVG", "white", "circle", "green");
+      const shape = logo.renderShape();
+      const text = logo.renderText();
+      const rendered = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n\t${shape}\n\t${text}\n</svg>`;
+      expect(logo.render()).toBe(rendered);
+    });
+  });
+});
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,1 +1,71 @@
 const Shapes = require("../lib/shapes.js");
+
+describe("Circle", () => {
+  // Test to verify that the Circle class can be instantiated.
+  describe("Instantiate", () => {
+    it("should be an instance of circle class", () => {
+      const circle = new Shapes.circle('white',0,0,10);
+      expect(circle).toBeInstanceOf(Shapes.circle);
+    });
+  });
+
+  // Test to verify output is rendered properly
+  describe("render()", () => {
+    it("should render circle properly", () => {
+      const rendered = 
+        '<circle cx="0" cy="0" r="5" fill="white" />';
+      const circle = new Shapes.circle('white',0,0,10);
+      expect(circle.render()).toBe(rendered);
+    });
+  });
+});
+
+describe("Square", () => {
+  // Test to verify that the Square class can be instantiated.
+  describe("Instantiate", () => {
+    it("should be an instance of square class", () => {
+      const square = new Shapes.square("white", 0, 0, 10);
+      expect(square).toBeInstanceOf(Shapes.square);
+    });
+  });
+
+  // Test to verify output is rendered properly
+  describe("render()", () => {
+    it("should render square properly", () => {
+      const rendered =
+        '<rect x="0" y="0" width="10" height="10" fill="white" />';
+      const square = new Shapes.square("white", 0, 0, 10);
+      expect(square.render()).toBe(rendered);
+    });
+  });
+});
+
+describe("Triangle", () => {
+  // Test to verify that the Triangle class can be instantiated.
+  describe("Instantiate", () => {
+    it("should be an instance of triangle class", () => {
+      const triangle = new Shapes.triangle("white", 0, 0, 10);
+      expect(triangle).toBeInstanceOf(Shapes.triangle);
+    });
+  });
+
+  // Test to verify the triangle points are calculated properly with shallow equality
+  describe("render()", () => {
+    it("should render triangle properly", () => {
+      const triangle = new Shapes.triangle("white", 0, 0, 10);
+      const points = ["0,5", "-5.773502691896258,-5", "5.773502691896258,-5"];
+      expect(triangle.getPoints()).toStrictEqual(points);
+    });
+  });
+
+  // Test to verify output is rendered properly
+  describe("render()", () => {
+    it("should render triangle properly", () => {
+      const triangle = new Shapes.triangle("white", 0, 0, 10);
+      const points = triangle.getPoints();
+      const rendered =
+        `<polygon points="${points[0]} ${points[1]} ${points[2]}" fill="white"/>`;
+      expect(triangle.render()).toBe(rendered);
+    });
+  });
+});
